9174814a74dc712cbef7750cf69c4c8c2df7e30f
DrStaf <aaron@staffordfarm.com.au>
2023-06-07 20:32:39 -0400

    Delete script directory

--- script/runner.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-git clone --branch issue-802-part-1 https://github.com/lumin-leaper/com.unity.webrtc --depth=1
-pushd com.unity.webrtc
-mkdir -p out/Debug
-mkdir -p out/Release
-bash -e BuildScripts~/build_libwebrtc_android.sh
-cp /com.unity.webrtc/artifacts/webrtc-android.zip /result/.
-popd
