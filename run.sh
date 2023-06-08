338df74fe434434c8bc92f816595d96791818204
DrStaf <aaron@staffordfarm.com.au>
2023-06-07 21:00:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ mkdir -p /result
 pushd libwebrtc-docker-builder
 sudo ./build.sh
 sudo ./run.sh
-sudo docker system prune # need to start with a bigger disk to avoid this. this is really a disk saving measure.
 popd
 git clone https://github.com/aaron-stafford/unity-webrtc-plugin-docker-builder
 pushd unity-webrtc-plugin-docker-builder
