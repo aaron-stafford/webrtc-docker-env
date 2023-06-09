c8ed2b8ade438be396e30d54961ec365dd8dd753
DrStaf <dark-apple@nacious.net>
2023-06-09 18:19:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ set -x
 git clone https://github.com/aaron-stafford/libwebrtc-docker-builder
 mkdir -p /result
 pushd libwebrtc-docker-builder
-sudo ./build.sh
-sudo ./run.sh
+./build.sh
+./run.sh
 popd
 git clone https://github.com/aaron-stafford/unity-webrtc-plugin-docker-builder
 pushd unity-webrtc-plugin-docker-builder
-sudo ./build.sh
-sudo ./run.sh
+./build.sh
+./run.sh
 popd
