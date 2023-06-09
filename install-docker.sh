b60cf336e5103d209e4c82a78c3253efee8f47c0
DrStaf <dark-apple@nacious.net>
2023-06-09 17:59:39 -0400

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -13,3 +13,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo service docker restart
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+newgrp docker
