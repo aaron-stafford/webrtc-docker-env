2a40c5d34b9ca9eae97d30d91b70cbcd1c7c85a3
DrStaf <dark-apple@nacious.net>
2023-06-10 11:56:07 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -e
 set -x
-export LIBWEBRTC_REPO=https://github.com/lumin-leaper/com.unity.webrtc
-export LIBWEBRTC_REPO_BRANCH=bugfix-depot_tools-issue-workaround
+export LIBWEBRTC_REPO=https://github.com/aaron-stafford/com.unity.webrtc
+export LIBWEBRTC_REPO_BRANCH=simple-x86-example
 git clone https://github.com/aaron-stafford/libwebrtc-docker-builder
 mkdir -p result
 pushd libwebrtc-docker-builder
 ./build.sh
 ./run.sh
 popd
-export WEBRTC_PLUGIN_REPO=https://github.com/lumin-leaper/com.unity.webrtc
-export WEBRTC_PLUGIN_REPO_BRANCH=main
+export WEBRTC_PLUGIN_REPO=https://github.com/aaron-stafford/com.unity.webrtc
+export WEBRTC_PLUGIN_REPO_BRANCH=simple-x86-example
 git clone https://github.com/aaron-stafford/unity-webrtc-plugin-docker-builder
 pushd unity-webrtc-plugin-docker-builder
 ./build.sh
